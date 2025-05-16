a98a9c41ec89b7a88efc8065109905e051a87ebc
PHC-Patatas2 <acctforthesis@gmail.com>
2025-05-16 11:02:27 +0800

    batch file for automation of files

---
+++ start-app.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Start XAMPP Apache and PostgreSQL (if using XAMPP's Control Panel)
+start "" "C:\xampp\xampp-control.exe"
+
+REM Wait for user to start Apache and PostgreSQL manually
+
+echo.
+echo Please ensure Apache and PostgreSQL are running in XAMPP Control Panel.
+echo Then open your browser and go to: http://localhost/fssv2/public/
+pause
